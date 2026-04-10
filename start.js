651159e37bba23db6177d30643b8ce154b173eb3
whatismean1-ai <whatismean1@gmail.com>
2026-04-11 04:12:48 +0900

    Add files via upload

---
+++ start.js
@@ -0,0 +1,32 @@
+(function () {
+  const path = window.location.pathname.split("/").pop().toLowerCase();
+
+  const startUserMap = {
+    "start.html": "indexs",
+    "starta.html": "indexsa",
+    "startb.html": "indexsb",
+    "startc.html": "indexsc",
+    "startd.html": "indexsd",
+    "starte.html": "indexse",
+    "startf.html": "indexsf",
+    "startg.html": "indexsg",
+    "starth.html": "indexsh",
+    "starti.html": "indexsi",
+    "startj.html": "indexsj",
+    "startk.html": "indexsk",
+    "startl.html": "indexsl"
+  };
+
+  const selectedUser = startUserMap[path] || "indexs";
+
+  try {
+    localStorage.setItem("selectedUser", selectedUser);
+    sessionStorage.setItem("selectedUser", selectedUser);
+  } catch (e) {}
+
+  const delay = 700; // 0.5~1초 사이
+
+  window.setTimeout(() => {
+    window.location.replace("./index.html");
+  }, delay);
+})();
