77c5fdb1e1a565ed8dc96aae2cc054ffdf6bd993
whatismean1-ai <whatismean1@gmail.com>
2026-04-12 01:27:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -716,18 +716,6 @@ document.addEventListener("DOMContentLoaded", () => {
       profileImage: "./assets/sun.jpeg"
     },
 
-    indexsm: {
-     name: "양연하",
-      rrnFront: "890717",
-      rrnBackMasked: "*******",
-      rrnBackFull: "2552616",
-      region: "광주광역시 북구",
-      address: "문화소통로 108번길 8-3, 초원빌라 4층",
-      issueDate: "2021.05.10",
-      issuer: "광주광역시 북구청장",
-      profileImage: "./assets/sun.jpeg"
-    },
-
     indexsg: {
       name: "박시원",
       rrnFront: "040311",
@@ -810,6 +798,18 @@ document.addEventListener("DOMContentLoaded", () => {
       issueDate: "2021.05.10",
       issuer: "광주광역시 광산구청장",
       profileImage: "./assets/hyo.jpeg"
+    },
+    
+    indexsm: {
+     name: "양연하",
+      rrnFront: "890717",
+      rrnBackMasked: "*******",
+      rrnBackFull: "2552616",
+      region: "광주광역시 북구",
+      address: "문화소통로 108번길 8-3, 초원빌라 4층",
+      issueDate: "2021.05.10",
+      issuer: "광주광역시 북구청장",
+      profileImage: "./assets/sun.jpeg"
     },
   };
 
